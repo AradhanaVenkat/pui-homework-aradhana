45173a19ef6ff4d9e742e7b7ced9f6855061c6d4
Aradhana <aradhanv@andrew.cmu.edu>
2022-12-03 02:22:45 -0500

    FP Version 2

--- Final Project/Javascript/main.js
+++ Final Project/Javascript/main.js
@@ -1,4 +1,4 @@
-// Pop up on card click
+// Pop up on confirmation bias card click
 function onConfirmationCardClick(){
     var x = document.getElementById("confirmationbiasdetails");
     console.log(x.style.display);
@@ -10,23 +10,54 @@ function onConfirmationCardClick(){
     console.log("cardClick")
 }
 
+// Closing pop up with confirmation bias details
+function onExitOneClick(){
+    var x = document.getElementById("confirmationbiasdetails");
+    x.style.display = "none";
+}
+
+
+
+// Pop up on decoy effect card click
 function onDecoyCardClick(){
     var x = document.getElementById("decoyeffectdetails");
-    if (x.style.display === "none") {
-      x.style.display = "block";
-    } else {
+    if (x.style.display === "block") {
       x.style.display = "none";
+    } else {
+      x.style.display = "block";
     }
 }
 
+// Closing pop up with decoy effect details
+function onExitTwoClick(){
+    var x = document.getElementById("decoyeffectdetails");
+    x.style.display = "none";
+}
+
+
+
+// Pop up on anchoring effect card click
 function onAnchorCardClick(){
     var x = document.getElementById("anchoringbiasdetails");
-    if (x.style.display === "none") {
-      x.style.display = "block";
-    } else {
+    if (x.style.display === "block") {
       x.style.display = "none";
+    } else {
+      x.style.display = "block";
     }
 }
+
+// Closing pop up with confirmation bias details
+function onExitThreeClick(){
+    var x = document.getElementById("anchoringbiasdetails");
+    x.style.display = "none";
+}
+
+
+// Animate.style - All animations will take half the time to accomplish
+// document.documentElement.style.setProperty('--animate-duration', '.8s');
+
+
+
 
 
 function onAnswer1Click() {
